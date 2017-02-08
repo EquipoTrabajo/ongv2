c12528f73373fee9668791e72c6fbce8736dd6b6
Josías D. Rodríguez <yosiprompt@gmail.com>
2017-02-08 15:16:06 -0400

    profiling

--- app/middleware/facebook-auth.js
+++ app/middleware/facebook-auth.js
@@ -6,24 +6,41 @@ const fbStrategy = new Strategy({
     clientID: process.env.FB_CLIENTID,
     clientSecret: process.env.FB_CLIENTSECRET,
     callbackURL: 'http://localhost:3000/login/facebook/return',
-    profileFields: ['id','name','cover','picture', 'birthday', 'location','gender', 'education','friends', 'likes{name,category}']
+    profileFields: ['id','name','cover','picture', 'birthday', 'location','gender', 'education','friends', 'likes.limit(50){name,created_time,category,category_list,fan_count,location}'],
+    profileURL: 'https://graph.facebook.com/v2.8/me'
   },
   function(accessToken, refreshToken, profile, callback) {
     Person.findOne({ 'facebookid' : profile.id }).exec()
       .then((person) => {
         let response = profile._json;
-        let address= response.location.name.split(',');
+        /*let firstName = response.first_name || '';
+        let middleName = response.middle_name || '';
+        let lastName = response.last_name || '';
+        let profilePicture = response.picture.data.url || undefined;
+        let coverPicture = response.cover.source || undefined;
+        let address = response.location.name.split(',') || [undefined,undefined,undefined];
+        let friends = response.friends.data || undefined;
+*/
+        console.log(response);
+        let address= [];
+        if (response.location) {
+          address = response.location.name.split(',');
+        } else {
+          address = ' , , '.split(',');
+        }
         if(person){
           //return callback(null, person);
           person.name = response.first_name + ' ' + response.middle_name + ' ' + response.last_name;
           person.profile_picture = response.picture.data.url;
           person.cover_picture = response.cover.source;
+          person.birthday = response.birthday;
+          person.gender = response.gender
           person.address['city'] = address[0];
           person.address['state'] = address[1];
           person.address['country'] = address[2];
           person.facebookid = response.id;
           person.followed_people = response.friends.data;
-          person.likes_fb = JSON.stringify(response.likes.data);
+          person.likes_fb = JSON.stringify(response.likes.data, null, ' ');
           
           person.save((err, result) => {
             if (err) {
@@ -55,7 +72,7 @@ const fbStrategy = new Strategy({
               "gender": response.gender,
               "email": response.email,
               "followed_people": response.friends.data,
-              "likes_fb": JSON.stringify(response.likes.data)
+              "likes_fb": JSON.stringify(response.likes.data, null, ' ')
             }
           );
           person.save()
